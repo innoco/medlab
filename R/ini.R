95eaa860bc3768f874714dd3199439eaf200b81a
Niels Kjær Stampe <niels.kjaer.stampe@regionh.dk>
2019-09-11 10:06:37 +0200

    new

--- R/ini.R
+++ R/ini.R
@@ -1,38 +1,20 @@
 
-pcks <- c('devtools', 'roxygen2', 'testthat', 'knitr', 'usethis')
-
-landscape_setup <- function(pkg){
-  new_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new_pkg))
-    install.packages(new_pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-landscape_setup(pcks)
-
 
 # check that you have everything installed and working by running the following code:
-has_devel()
+#has_devel()
 
 # devtools::create('escn')
 
 # This functions will populate the correct fields with some of the most common licenses
 # usethis::use_cc0_license('Simon Kønig')
 
-devtools::load_all()
+#devtools::load_all()
 
 # This function generates the documentation (.Rd) files and then stores them in the man subdirectory.
-devtools::document()
+#devtools::document()
 
 #  install new package and check out the new documentation
-devtools::install()
-
-
-
-
-
-
-
+#devtools::install()
 
 
 
@@ -40,9 +22,10 @@ devtools::install()
 
 
 # Adds a dummy .R file that will prompt roxygen to generate basic package-level documentation.
-usethis::use_package_doc()
+
+#usethis::use_package_doc()
 
 # updating documentation
-devtools::document()
+#devtools::document()
 
 
--- R/preamble.R
+++ R/preamble.R
@@ -1,13 +1,14 @@
 # Load sequence ------------------------------------------------------------------------------
 ### Clean global envroment
-rm(list=ls()) + cat("\014")                                     # Removes all variables
-if(!is.null(dev.list())){dev.off(dev.list()["RStudioGD"])}      # Clean plots
+clean_global <- function(){
+  rm(list=ls()) + cat("\014")                                     # Removes all variables
+  if(!is.null(dev.list())){dev.off(dev.list()["RStudioGD"])}      # Clean plots
+}
 
 ### Load libraries
-load.libraries <- c('devtools', 'prodlim', 'Publish', 'lubridate', 'plyr', 'dplyr',
-                    'stringr', 'ggplot2', 'reshape2', 'xtable', 'tableone',
-                    'survival', 'GGally', 'scales')
-install.lib <- load.libraries[!load.libraries %in% installed.packages()]
-for(libs in install.lib) install.packages(libs, dependencies = TRUE)
-sapply(load.libraries, require, character = TRUE)
-rm(load.libraries, install.lib, libs)
+landscape_setup <- function(load.libraries){
+  install.lib <- load.libraries[!load.libraries %in% installed.packages()]
+  for(libs in install.lib) install.packages(libs, dependencies = TRUE)
+  sapply(load.libraries, require, character = TRUE)
+  rm(load.libraries, install.lib, libs)
+}
